5dccd524b135408b181b688f71022489c73d0441
Marcel Grüßinger <marcel.gruessinger@web.de>
2018-03-26 16:01:56 +0200

    Added debug message.

--- TrafficLightSimulation/trafficlightview.cpp
+++ TrafficLightSimulation/trafficlightview.cpp
@@ -1,11 +1,17 @@
 #include "trafficlightview.h"
 #include "ui_trafficlightview.h"
 
-TrafficLightView::TrafficLightView(QWidget *parent) :
+TrafficLightView::TrafficLightView(QWidget* parent) :
     QMainWindow(parent),
     ui(new Ui::TrafficLightView)
 {
     ui->setupUi(this);
+
+    ui->menuItemExit->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_E));
+    ui->menuItemAbout->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_A));
+
+    connect(ui->menuItemAbout, SIGNAL(triggered(bool)), this, SLOT(showAboutDialog()));
+    connect(ui->menuItemExit, SIGNAL(triggered(bool)), this, SLOT(stopApplication()));
 
     scene = new QGraphicsScene(this);
     ui->graphicsView->setScene(scene);
@@ -28,9 +34,22 @@ void TrafficLightView::drawTrafficLights(QColor redLight, QColor yellowLight, QC
     QBrush greenBrush(greenLight, Qt::SolidPattern);
     QPen pen(Qt::black);
 
+    scene->clear();
     scene->addEllipse(40, 25, 150, 150, pen, redBrush);
     scene->addEllipse(40, 200, 150, 150, pen, yellowBrush);
     scene->addEllipse(40, 375, 150, 150, pen, greenBrush);
 
-    //std::cout << "Redrawn traffic lights.";
+    std::cout << "Re-drawn traffic lights\n";
+}
+
+void TrafficLightView::showAboutDialog()
+{
+    QMessageBox::information(this,
+                             tr("Traffic Light Simulation"),
+                             tr("This application was developed by Marcel Gruessinger."));
+}
+
+void TrafficLightView::stopApplication()
+{
+    QApplication::quit();
 }
